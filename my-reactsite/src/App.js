3121924267b323772407bdd1811b744941acaa0c
webDev-Learner <pawnmugi@gmail.com>
2024-05-13 21:22:20 +0530

    vscode setup is done

---
+++ my-reactsite/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-white flex flex-row justify-between w-24 items-center m-5">
+      <img src="logo512.png" className="h-10 animate-pulse"></img>
+      <h4 className="font-bold mx-2">REACT.</h4> 
+    </div>
+  );
+}
+
+export default App;
