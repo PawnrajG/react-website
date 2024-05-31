f690af824deeec2207511ea4157b7076d99671e6
webDev-Learner <pawnmugi@gmail.com>
2024-05-31 22:36:58 +0530

    Fix: fixed button classes

--- my-reactsite/src/components/Learn.js
+++ my-reactsite/src/components/Learn.js
@@ -8,7 +8,7 @@ const Learn = () =>{
                 <div className="text-black font-bold m-5 flex flex-col">
                     <h2 className="text-2xl md:text-4xl text-cyan-400 m-4">React for Beginners</h2>
                     <p className="text-slate-700 mx-4 mb-4 font-semibold">Before learning react you must me familiar with the basics of HTML, CSS and javascript and ensure you are done with some projects.</p>
-                    <a href="https://react.dev/learn" className="w-[150px] border-2 border-black text-black rounded-full px-5 py-2 m-4 tranform hover:scale-110 transition ease-in-out duration-300">Start Learning</a>
+                    <a href="https://react.dev/learn" className="w-[150px] border-2 border-black text-black rounded-full px-5 py-2 m-4 hover:scale-110 transition ease-in-out duration-300">Start Learning</a>
                 </div>
             </div>  
         </div>
