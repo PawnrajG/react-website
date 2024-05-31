050e81d7a84cbbacc50707f1278eaa104440f7c3
webDev-Learner <pawnmugi@gmail.com>
2024-05-31 22:37:50 +0530

    Feat: added the newsletter component to App component

--- my-reactsite/src/App.js
+++ my-reactsite/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Learn from "./components/Learn";
+import Newsletter from "./components/Newsletter";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <Learn/>
+      <Newsletter/>
     </>
   );
 }
