758a042cc809011d76baff0d0d4b358b9aa0dc42
webDev-Learner <pawnmugi@gmail.com>
2024-06-02 22:31:32 +0530

    Fix: altered css

--- my-reactsite/src/components/Hero.js
+++ my-reactsite/src/components/Hero.js
@@ -7,8 +7,8 @@ const Hero = () =>{
                 <img src="logo512.png" className="h-24 md:h-32 animate-bounce"/>
                 <h2 className="uppercase md:text-2xl text-sm text-cyan-300 m-2 p-1">To learn about the react</h2>
                 <h1 className="md:text-6xl text-xl w-auto text-white m-2 p-1">Create Web in React.</h1>
-                <ReactTyped className="text-sm md:text-2xl md:max-w-full max-w-lg text-white m-2 p-1 self-center" strings={["A fast and interactive user interface."," A fast and interactive user experience."]} typeSpeed={100} backSpeed={50} loop></ReactTyped>
-                <p className="md:max-w-full max-w-lg md:text-xl text-xs w-auto text-gray-600 mt-2 p-1 self-center">It's widely used in both small projects and large-scale applications.</p>
+                <ReactTyped className="text-sm md:text-2xl md:max-w-full max-w-lg text-white m-2 p-1 self-center text-center" strings={["A fast and interactive user interface."," A fast and interactive user experience."]} typeSpeed={100} backSpeed={50} loop></ReactTyped>
+                <p className="md:max-w-full max-w-lg md:text-xl text-xs w-auto text-gray-600 mt-2 p-1 self-center text-center">It's widely used in both small projects and large-scale applications.</p>
                 <a href="https://react.dev" className="border-2 rounded-full px-5 py-2 my-10 tranform hover:scale-110 transition ease-in-out duration-300">Get Started</a>
             </div>
         </div>
