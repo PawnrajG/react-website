18d5911270fe498ba32aa9638bb40064dfbe19d7
webDev-Learner <pawnmugi@gmail.com>
2024-06-02 22:01:43 +0530

    Feat: Techcards component is added to App component

--- my-reactsite/src/App.js
+++ my-reactsite/src/App.js
@@ -3,13 +3,15 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Learn from "./components/Learn";
 import Newsletter from "./components/Newsletter";
+import Techcards from "./components/Techcards";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
-      <Learn/>
-      <Newsletter/>
+      <Learn />
+      <Newsletter />
+      <Techcards />
     </>
   );
 }
