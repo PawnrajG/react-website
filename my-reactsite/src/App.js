5b986f98d61e772bfbf41486f9b3620d0799902f
webDev-Learner <pawnmugi@gmail.com>
2024-06-06 20:10:14 +0530

    Fix: Rectified error

--- my-reactsite/src/App.js
+++ my-reactsite/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Learn from "./components/Learn";
